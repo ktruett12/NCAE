fa3d8eee4fc06b6df765b65c03a4ae250bc22e7c
ktruett12 <159093216+ktruett12@users.noreply.github.com>
2024-02-06 18:16:25 -0500

    Update delusers2.sh

--- Scripts/User Management/delusers2.sh
+++ Scripts/User Management/delusers2.sh
@@ -16,7 +16,7 @@ IFS=',' read -r -a keep_usernames <<< "$keep_usernames_input"
 # Delete non-system user accounts
 getent passwd | while IFS=: read -r username _ uid _ ; do
     # Check if the user is not root, not a system account, and not in the list of usernames to keep
-    if [ "$username" != "root" ] [ "$username" != "nobody" ] && [ "$uid" -ge 1000 ] && ! [[ " ${keep_usernames[@]} " =~ " $username " ]]; then
+    if [ "$username" != "root" ] && [ "$username" != "nobody" ] && [ "$uid" -ge 1000 ] && ! [[ " ${keep_usernames[@]} " =~ " $username " ]]; then
         # Delete the user
         userdel -r "$username" >/dev/null 2>&1
         echo "User '$username' deleted."
